006d358234e85194c540d79acedc18d12a87544e
Luke Goncalves <psxlg6@nottingham.ac.uk>
2025-12-03 22:00:16 +0000

    explain code better

--- day3/part2.py
+++ day3/part2.py
@@ -28,10 +28,19 @@ input = open("input.txt").read().split()
 
 #     jolt_sort()
 
-def chk(ss,k):
-    if k==0:return ''
-    mx=max(ss[:len(ss)-k+1])
-    pm=ss.index(mx)
-    return mx+chk(ss[pm+1:],k-1)
-ans=sum(int(chk(s,12)) for s in input)
-print('part2',ans)
+def battery_sort(bank, num_of_batteries):
+
+    if num_of_batteries == 0:
+        return '' # if we get to the end of the battery bank, end the recursive loop
+    
+    max_range = len(bank) - num_of_batteries + 1 # the index to be the end of the range. 1st loop: range = 100 - 13 = 87, 2nd loop: remaining bank - 12, etc.
+    max_value = max(bank[:max_range]) # find the largest number (highest joltage) in the range
+    position = bank.index(max_value) # the position of the largest number
+    remaining_bank = bank[position + 1:] # the rest of the bank from the position of the number after largest number
+
+    highest_total_joltage = max_value + battery_sort(remaining_bank, num_of_batteries - 1) # recursive call, return max value + the max value from the next loop, until end of sequence (bank)
+    if len(highest_total_joltage) == 12: print(highest_total_joltage)
+    return highest_total_joltage
+
+joltage = sum(int(battery_sort(bank, 12)) for bank in input) # add up all the highest joltages for the answer
+print('Joltage:', joltage)
